75dc9a541fb04cb0ee9a010061d964dd8fb99a70
salehe mwanga <salehe.mwanga@ega.go.tz>
2025-04-27 10:03:24 +0300

    step-15

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -22,12 +22,6 @@ class _NotesViewState extends State<NotesView> {
   }
 
   @override
-  void dispose() {
-    _notesService.close();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -76,19 +70,27 @@ class _NotesViewState extends State<NotesView> {
                     case ConnectionState.waiting:
                     case ConnectionState.active:
                       if (snapshot.hasData) {
-                        final allNotes = snapshot.data!;
+                        final allNotes = snapshot.data as List<DatabaseNote>;
                         return ListView.builder(
                           itemCount: allNotes.length,
                           itemBuilder: (context, index) {
                             final note = allNotes[index];
-                            return ListTile(title: Text(note.text));
+                            return ListTile(
+                              title: Text(
+                                note.text,
+                                maxLines: 1,
+                                softWrap: true,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            );
                           },
                         );
                       } else {
-                        return const Text('No notes found');
+                        return Center(child: const Text('No notes found'));
                       }
                     default:
-                      return const CircularProgressIndicator();
+                      print('Error: ${snapshot.hasData}');
+                      return Center(child: const CircularProgressIndicator());
                   }
                 },
               );
