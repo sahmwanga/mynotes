4b8b883c267984cfe946f3f6a93ef5c6f02ce641
salehe mwanga <salehe.mwanga@ega.go.tz>
2025-04-27 10:58:22 +0300

    step-16

---
+++ lib/utilities/dialogs/delete_dialog.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+import 'package:learningdart/utilities/dialogs/generic_dialog.dart';
+
+Future<bool> showDeleteDialog(BuildContext context) {
+  return showGenericDialog<bool>(
+    context: context,
+    title: 'Delete',
+    content: 'Are you sure you want to delete this item?',
+    optionsBuilder: () => {'Cancel': false, 'Delete': true},
+  ).then((value) => value ?? false);
+}
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:learningdart/constants/routes.dart';
 import 'package:learningdart/services/auth/auth_exceptions.dart';
 import 'package:learningdart/services/auth/auth_service.dart';
-import 'package:learningdart/utilities/show_error_dialog.dart';
+import 'package:learningdart/utilities/dialogs/error_dialog.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
